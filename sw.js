a9d79294971d14a9b8f80b4ad5b59e5f6a2fea51
Jared Mortenson <cjared.mortenson@gmail.com>
2019-04-24 20:58:10 -0600

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1ea1cc81a880aa5905af.js"
+    "url": "webpack-runtime-33eb2caf5ed86a7864a9.js"
   },
   {
     "url": "styles.1025963f4f2ec7abbad4.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a246d651ad7dcf2be7389b2a2472a8d4"
+    "revision": "a1ff3f38f80fddbb8afe6b3f2afbdfa5"
   },
   {
     "url": "component---src-pages-404-js-4d8be3760dea49947e9a.js"
