a4d566e159cac7ec91a833b10a5d19061ced2577
Jared Mortenson <cjared.mortenson@gmail.com>
2019-06-20 13:29:57 -0600

    Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-142492963-1',
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
