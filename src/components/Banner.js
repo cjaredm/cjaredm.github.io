631c9854b6e17ef6687a9518d41c0774c4be211f
Jared Mortenson <cjared.mortenson@gmail.com>
2019-08-14 14:29:00 -0600

    Profile pic! woot woot

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,35 +1,44 @@
-import React from 'react'
-import { ContactIcons } from './ContactIcons'
+import React from 'react';
+import { ContactIcons } from './ContactIcons';
+import profile from '../assets/images/profile.jpeg';
 
 const Banner = props => (
   <section id="banner" className="major">
     <div className="inner">
-      <header className="major">
-        <h1>I'm Jared, a Frontend Developer</h1>
-      </header>
-      <div className="content">
-        <div>
-          <p>
-            <strike>
-              I focus on elegant, clean and efficient code, blah blah...
-            </strike>
-            <br />
-            Let's cut the BS. I balance functionality, learning and getting
-            things done.
-          </p>
-          <ContactIcons className="reset" />
+      <div className="banner-wrapper">
+        <div className="profile-wrapper">
+          <img className="profile-picture" src={profile} alt="profile picture" />
         </div>
-        <ul className="actions">
-          <li>
-            <a href="#one" className="button next scrolly">
-              {' '}
-              View My Work{' '}
-            </a>
-          </li>
-        </ul>
+
+        <div>
+          <header className="major">
+            <h1>I'm Jared, a Frontend Developer</h1>
+          </header>
+
+          <div className="content">
+            <div>
+              <p>
+                <strike>
+                  I focus on elegant, clean and efficient code, blah blah...
+                </strike>
+                <br />
+                Let's cut the BS. I balance functionality, learning and getting
+                things done.
+              </p>
+              <ContactIcons className="reset" />
+            </div>
+            <ul className="actions">
+              <li>
+                <a href="#one" className="button next scrolly">
+                  View My Work
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </section>
 );
 
-export default Banner
+export default Banner;
