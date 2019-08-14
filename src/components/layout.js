7eac5a01316fdfb1c324b94ccc399d3d1c0fb12e
Jared Mortenson <cjared.mortenson@gmail.com>
2019-08-13 23:16:45 -0600

    Unused var

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './Header';
 import Menu from './Menu';
-import Contact from './Contact';
 import Footer from './Footer';
 import '../assets/scss/main.scss';
 
